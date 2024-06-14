089ea8fec66eccd8d68bb177b73aa77435c146a1
MiohitoKiri5474 <lltzpp@gmail.com>
2024-06-14 15:35:36 +0800

    remove testing case

--- 2024-Spring-NCKU-CompilerHW3/test.cpp
+++
@@ -1,9 +0,0 @@
-int main ( string argv[] ) {
-    int apple = 12345;
-    for ( int i = 0 ; i < 10 ; i++ ) {
-        cout << "meow" << endl;
-    }
-    for ( int i = 5 ; i >= 0 ; i-- ) {
-        cout << "wolf" << endl;
-    }
-}
